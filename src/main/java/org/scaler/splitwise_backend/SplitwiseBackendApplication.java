047fa6606d5ac382caa84f6835877f1bf3fcef48
ankitRaghuwanshi <ankitraghuwanshi96@gmail.com>
2026-08-15 18:41:24 +0530

    starting

---
+++ src/main/java/org/scaler/splitwise_backend/SplitwiseBackendApplication.java
@@ -0,0 +1,13 @@
+package org.scaler.splitwise_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitwiseBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SplitwiseBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/scaler/splitwise_backend/SplitwiseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.scaler.splitwise_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitwiseBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
